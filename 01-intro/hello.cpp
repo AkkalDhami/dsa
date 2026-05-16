b4d1639d40d1f181578e05bca4a8d7ff0812fc1e
AkkalDhami <dhamiakkal21@gmail.com>
2026-05-16 10:28:07 +0545

    feat: add introductory C++ examples and .gitignore
    add .gitignore to ignore .exe files and VS Code workspace files
    add 01-intro/first.cpp as basic hello world program
    add 01-intro/hello.cpp with multiple output methods and simple arithmetic

---
+++ 01-intro/hello.cpp
@@ -0,0 +1,18 @@
+//? Day-01: May 14 2026
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello World!" << endl; //* '<<' => insertion operator
+    cout << "Hello Akkal!\n";
+
+    printf("Hello Akkal!\n");
+
+    cout << 3 * 4;
+
+    return 0;
+}
