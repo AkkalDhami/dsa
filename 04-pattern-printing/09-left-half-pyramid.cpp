131c8c9734c46235e84ceb4eed81bb60168afbef
AkkalDhami <dhamiakkal21@gmail.com>
2026-05-18 09:00:33 +0545

    feat(pattern-printing): add left half pyramid pattern printing program
    implements the pattern using nested loops to print leading spaces and star sequences

---
+++ 04-pattern-printing/09-left-half-pyramid.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+         *
+       * *
+     * * *
+   * * * *
+ * * * * *
+ 
+ */
+
+int main() {
+    int n = 5;
+    for (int r = 1; r <= n; r++)
+    {
+        for (int c = 1; c <= n - r; c++)
+        {
+            cout << "  ";
+        }
+
+        for (int c = 1; c <= r; c++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}
