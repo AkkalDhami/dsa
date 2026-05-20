c1394f30ae84f2854ffe54173a69218dabcc3cc7
AkkalDhami <dhamiakkal21@gmail.com>
2026-05-20 10:59:42 +0545

    feat: add butterfly and hollow butterfly patterns
    Add two new C++ source files under the pattern-printing directory, implementing the classic star butterfly and hollow butterfly printing patterns, each with a commented sample output preview at the top of the file.

---
+++ 04-pattern-printing/19-hollow-butterfly-pattern.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+* * * * * * * * * * 
+* * * *     * * * * 
+* * *         * * * 
+* *             * * 
+*                 * 
+*                 * 
+* *             * * 
+* * *         * * * 
+* * * *     * * * * 
+* * * * * * * * * * 
+
+*/
+
+int main()
+{
+    int n = 15;
+
+    for (int r = n; r >= 1; r--)
+    {
+        for (int c = 1; c <= r; c++)
+        {
+            cout << "* ";
+        }
+        for (int c = 1; c <= n - r; c++)
+        {
+            cout << "    "; // 4 spaces
+        }
+
+        for (int c = r; c >= 1; c--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    for (int r = 1; r <= n; r++)
+    {
+        for (int c = 1; c <= r; c++)
+        {
+            cout << "* ";
+        }
+        for (int c = 1; c <= n - r; c++)
+        {
+            cout << "    "; // 4 spaces
+        }
+
+        for (int c = r; c >= 1; c--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ 04-pattern-printing/20-butterfly-pattern.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+*                 *
+* *             * *
+* * *         * * *
+* * * *     * * * *
+* * * * * * * * * *
+* * * *     * * * *
+* * *         * * *
+* *             * *
+*                 *
+
+*/
+
+int main()
+{
+    int n = 5;
+    
+    for (int r = 1; r <= n-1; r++)
+    {
+        for (int c = 1; c <= r; c++)
+        {
+            cout << "* ";
+        }
+        for (int c = 1; c <= n - r; c++)
+        {
+            cout << "    "; // 4 spaces
+        }
+
+        for (int c = r; c >= 1; c--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
+    for (int r = n; r >= 1; r--)
+    {
+        for (int c = 1; c <= r; c++)
+        {
+            cout << "* ";
+        }
+        for (int c = 1; c <= n - r; c++)
+        {
+            cout << "    "; // 4 spaces
+        }
+
+        for (int c = r; c >= 1; c--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
